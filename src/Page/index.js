3fbe56ce2ba588a76793282db527e98b48cccdd8
Phuc Nguyen <p30012k1@gmail.com>
2022-11-14 10:51:53 +0700

    up

---
+++ src/Page/index.js
@@ -0,0 +1,41 @@
+import { StatusBar } from "expo-status-bar"
+import { useRef, useEffect } from "react"
+import { StyleSheet, View, BackHandler } from "react-native"
+
+import WebView from "react-native-webview"
+
+const HomeScreen = ({ navigation }) => {
+  const URL_WEBVIEW = 'https://www.phuckhanggem.com/'
+
+  const webViewRef = useRef()
+  useEffect(() => {
+    const backAction = () => {
+      webViewRef?.current?.goBack()
+      return true
+    }
+
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      backAction
+    )
+
+    return () => backHandler.remove()
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style={"auto"} />
+      <WebView
+        ref={webViewRef}
+        style={styles.container}
+        source={{ uri: URL_WEBVIEW }}
+      />
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: { flex: 1 }
+})
